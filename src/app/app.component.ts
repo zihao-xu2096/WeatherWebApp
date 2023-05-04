cd2e22b33916d38521d0ba9c484f5597e0592f71
zihao-xu2096 <harryxcz@gmail.com>
2023-05-04 11:45:13 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WeatherService } from './services/weather.service';
+import { WeatherData } from './models/weather.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sample_angular';
+export class AppComponent implements OnInit{
+  
+  constructor(private weatherService: WeatherService){
+  }
+  cityName: string = 'Wellington'; // Define a variable to store the city name for search
+  weatherData?: WeatherData; // Store the weatherdata json object obtained from the API
+
+  ngOnInit(): void {
+      this.getWeatherData(this.cityName);
+      this.cityName = ''
+  }
+
+  onSubmit() {
+    this.getWeatherData(this.cityName);
+    this.cityName = '';
+  }
+
+  private getWeatherData(cityName: string){
+    this.weatherService.getWeatherData(cityName).subscribe({
+      next: (response) => {
+        this.weatherData = response;
+
+        console.log(response)
+      }
+    })
+  }
+
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    weatherApiBaseUrl: 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather',
+    XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+    XRapidAPIHostHeaderValue: 'weather-by-api-ninjas.p.rapidapi.com',
+    XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+    XRapidAPIKeyHeaderValue: 'f0617a1334msh958d68a6a169efdp1ae03cjsnf72faaf95289'
+};
